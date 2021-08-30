3ea65821d6982d6c70479124d2f2632616fab84a
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2021-08-30 23:45:34 +0200

    dao package
    dao package

---
+++ AppointmentRepository.java
@@ -0,0 +1,18 @@
+package com.stc.appointment.dao;
+
+import com.stc.appointment.entity.Appointment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@CrossOrigin("http://localhost:4200")
+@Repository
+public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+
+    Page<Appointment> findByAppointmentId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Appointment> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+}
