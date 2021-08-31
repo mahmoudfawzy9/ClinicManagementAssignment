957fb7f3e882104fa1b5d54e18101f9deaefb4b0
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2021-08-31 11:09:33 +0200

    Update AppointmentRepository.java

--- AppointmentRepository.java
+++ AppointmentRepository.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+    
+    @Query("SELECT a FROM Appointment a WHERE a.name LIKE %?1%")
+    public List<Appointment> findAll(String keyword);
 
     Page<Appointment> findByAppointmentId(@RequestParam("id") Long id, Pageable pageable);
 
