fcbcb0f65c6c9c626a0b5c068cd302931c858f22
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2021-08-31 07:51:56 +0200

    dao package
    dao package

---
+++ AllergyRepository.java
@@ -0,0 +1,11 @@
+package com.stc.appointment.dao;
+
+import com.stc.appointment.entity.Allergy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface AllergyRepository extends JpaRepository<Allergy, Long> {
+}
